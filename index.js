be532920e9f7f157bec5fc3ecb888b6fdbfde6ea
Tommy White <tbw2.mail@gmail.com>
2022-02-22 17:51:04 -0700

    Add ticket check button. Add ticketed customers to list.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const customers = ['Tommy', 'Tess', 'James', 'Anna', 'Travis', 'Stefano', 'Gabe'
 const customerList = document.querySelector('.customerList');
 const ticket = document.querySelector('.ticket');
 const noTicket = document.querySelector('.noTicket');
+const checkBtn = document.querySelector('#checkBtn');
 
 // Sanitize customer array for display
 const sanCustomers = customers.map((customer) => {
@@ -10,3 +11,16 @@ const sanCustomers = customers.map((customer) => {
 });
 
 customerList.textContent = `${sanCustomers}`;
+
+// Ticket check button checks to see who has tickets
+checkBtn.addEventListener('click', handleCheckTickets);
+
+function handleCheckTickets() {
+  for (i = 0; i < customers.length; i++) {
+    if (customers[i] === 'Tess' || customers[i] === 'Stefano') {
+      noTicket.textcontent += `${customers[i]}, `;
+    } else {
+      ticket.textContent += `${customers[i]}, `;
+    }
+  }
+}
